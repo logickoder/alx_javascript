728645593c02712a425e5a1d29222d05bf38e012
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2023-09-06 08:41:25 +0100

    fix: 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
-        if (w > 0) {
+        if (w > 0 && h > 0) {
             this.width = w
-        }
-        if (h > 0) {
             this.height = h
         }
     }
